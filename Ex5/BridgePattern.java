1b10a870d67c8569307764ca2179a00adfacc4a2
thisIsMaamathi <125466717+thisIsMaamathi@users.noreply.github.com>
2023-03-30 08:43:30 +0530

    Commiting updated ReviewComments to Ex5

--- Ex5/BridgePattern.java
+++ Ex5/BridgePattern.java
@@ -1,48 +1,41 @@
 package Ex5;
 
 public class BridgePattern {
-
+//A structural design
+//Allows to seperate abstraction(shape) from implementation(Coloring)
 	public static void main(String args[]) {
-		// a square with red color
+		// object in the type of the abstract class created and the implementation is called along with it
 		Shape square = new Sqr(new Blue());
 		square.draw();
-
 	}
 }
 
-interface Color {
+//interface ie implementation of color 
+interface Coloring {
+	// this method is meant to fill color to a shape
 	void fill();
 }
 
-class Blue implements Color {
+class Blue implements Coloring {
 	@Override
-	
 	public void fill() {
 		System.out.println("Color is Blue");
-		
 	}
 }
 
+//the abstract class which represents shape
 abstract class Shape {
-	protected Color color;
-
-	
-
-	// standard constructors
-
+	protected Coloring color;
 	abstract public void draw();
 }
 
 class Sqr extends Shape {
-
-	public Sqr(Color color) {
-		this.color=color;
-		
+	public Sqr(Coloring color) {
+		this.color = color;
 	}
-
 	@Override
 	public void draw() {
-		System.out.println( "Square drawn");
+		System.out.println("Square drawn");
 		color.fill();
 	}
 }
--- Ex5/FlyweightPatterns.java
+++ Ex5/FlyweightPatterns.java
@@ -1,20 +1,22 @@
 package Ex5;
 
 import java.util.HashMap;
-
+//This pattern is mainly used to reduce number of objects created and increase performance
+//It is a structural pattern
 public class FlyweightPatterns {
+	
 	public static void main(String[] args) {
+		//creating yellow circle
 		Shapes yellowCircle = ShapeFactory.getCircle("YELLOW");
-		yellowCircle.draw("hi");
+		yellowCircle.draw();
+		//again creating yellow circle,but here new object is not created since yellow circle already exists
 		Shapes yellowCircle2 = ShapeFactory.getCircle("YELLOW");
-		yellowCircle2.draw("hi");
-		
+		yellowCircle2.draw();
+	    //new red circle is created since circle of red does not exists
 		Shapes redCircle = ShapeFactory.getCircle("Red");
-		yellowCircle.draw("hi");
-
-//		 
-//		
-//
+		yellowCircle.draw();
+        //here the hashcode of yellowCircle and yellowCircle2 is the same since no new circle is created
+		//thus here memory usage is optimized by restriction of similar objects' instantiation
 		System.out.println(yellowCircle.hashCode());
 		System.out.println(yellowCircle2.hashCode());
 
@@ -22,16 +24,18 @@ public class FlyweightPatterns {
 	}
 }
 
+//abstract class shape with methods to set color and draw a circle
 abstract class Shapes {
 	public abstract void setColor(String color);
-
-	public abstract void draw(String content);
+	public abstract void draw();
 }
 
+//different type of available shapes declared with enum
 enum ShapeType {
 	SQUARE, CIRCLE, TRIANGLE
 }
 
+//concrete class implementation of the shapes class 
 class Square extends Shapes {
 
 	final ShapeType shape = ShapeType.SQUARE;
@@ -42,7 +46,7 @@ class Square extends Shapes {
 	}
 
 	@Override
-	public void draw(String content) {
+	public void draw() {
 		System.out.println("Drawing Square in " + color);
 	}
 }
@@ -57,11 +61,10 @@ class Circle extends Shapes {
 	}
 
 	@Override
-	public void draw(String content) {
+	public void draw() {
 		System.out.println("Drawing Circle in " + color);
 	}
 }
-
 
 class Triangle extends Shapes {
 
@@ -73,63 +76,64 @@ class Triangle extends Shapes {
 	}
 
 	@Override
-	public void draw(String content) {
+	public void draw() {
 		System.out.println("Drawing triangle in " + color);
 	}
 }
 
-	class ShapeFactory
+//Shapes factory to create the shapes in mentioned type of color
+class ShapeFactory
 
-	{
-		private static final HashMap<String, Shapes> shapesMap = new HashMap<>();
-
-		public static Shapes getSquare(String color) {
-			String key = color + "-SQUARE";
-
-			Shapes shape = shapesMap.get(key);
-
-			if (shape != null) {
-				return shape;
-			} else {
-				shape = new Square();
-				shape.setColor(color);
-				shapesMap.put(key, shape);
-			}
-
+{   //Hashmap is used for optimized creation of objects
+	private static final HashMap<String, Shapes> shapesMap = new HashMap<>();
+    //creates the shape object and assigns color to it
+	public static Shapes getSquare(String color) {
+		//assigning key value and passing to hashmap
+		String key = color + "-SQUARE";
+		Shapes shape = shapesMap.get(key);
+        //checking if the shape of the same type and color exists
+		if (shape != null) {
+			//if it exists,returning the existing shape
 			return shape;
+		} 
+		else {//If it doesn't exist,creating a new shape
+			shape = new Square();
+			shape.setColor(color);
+			shapesMap.put(key, shape);
 		}
-
-		public static Shapes getCircle(String color) {
-			String key = color + "-CIRCLE";
-
-			Shapes shape = shapesMap.get(key);
-
-			if (shape != null) {
-				return shape;
-			} else {
-				shape = new Square();
-				shape.setColor(color);
-				shapesMap.put(key, shape);
-			}
-
-			return shape;
-		}
-
-		public static Shapes getTriangle(String color) {
-			String key = color + "-TRIANGLE";
-
-			Shapes shape = shapesMap.get(key);
-
-			if (shape != null) {
-				return shape;
-			} else {
-				shape = new Square();
-				shape.setColor(color);
-				shapesMap.put(key, shape);
-			}
-
-			return shape;
-		}
+        
+		return shape;
 	}
 
+	public static Shapes getCircle(String color) {
+		String key = color + "-CIRCLE";
 
+		Shapes shape = shapesMap.get(key);
+
+		if (shape != null) {
+			return shape;
+		} else {
+			shape = new Square();
+			shape.setColor(color);
+			shapesMap.put(key, shape);
+		}
+
+		return shape;
+	}
+
+	public static Shapes getTriangle(String color) {
+		String key = color + "-TRIANGLE";
+
+		Shapes shape = shapesMap.get(key);
+
+		if (shape != null) {
+			return shape;
+		} else {
+			shape = new Square();
+			shape.setColor(color);
+			shapesMap.put(key, shape);
+		}
+
+		return shape;
+	}
+}
